55bfeec5176cc36e6b0d866d6c265da834506518
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2020-02-19 18:59:15 +0100

    Aplikacja z okienkami VB.NET

--- Emerytura/ConsoleAppVisualBasic/Program.cs
+++ Emerytura/ConsoleAppVisualBasic/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 
-
-namespace ConsoleAppInteraktywnie
+namespace ConsoleAppVisualBasic
 {
     class Program
     {
@@ -14,19 +14,21 @@ namespace ConsoleAppInteraktywnie
             // tu piszemy kod
             Console.OutputEncoding = Encoding.UTF8;
 
-            
-            
+            Interaction.MsgBox("Programik Emerytura");
 
-            Console.WriteLine("Program EMERYTURA");
-            Console.Write("Podaj swoje imię: ");
+
+            //Console.WriteLine("Program EMERYTURA");
+            //Console.Write("Podaj swoje imię: ");
             string imie;
-            imie = Console.ReadLine().Trim().ToUpper();
+            //imie = Console.ReadLine().Trim().ToUpper();
             //imie = imie.Trim();
+            imie = Interaction.InputBox("Podaj imię ", "Tytuł okienka").Trim().ToUpper();
 
-            Console.Write("Podaj swoje nazwisko: ");
-            string nazwisko = Console.ReadLine().Trim();
-            
-            string pierwszaLitera = nazwisko.Substring(0,1);
+            //Console.Write("Podaj swoje nazwisko: ");
+            //string nazwisko = Console.ReadLine().Trim();
+            string nazwisko = Interaction.InputBox("Podaj nazwisko ", "Tytuł okienka");
+
+            string pierwszaLitera = nazwisko.Substring(0, 1);
             string reszta = nazwisko.Substring(1);
             nazwisko = pierwszaLitera.ToUpper() + reszta.ToLower();
 
@@ -34,24 +36,25 @@ namespace ConsoleAppInteraktywnie
             //Console.WriteLine("Witaj, {0} {1}!", nazwisko, imie); //formatted string
             Console.WriteLine($"Witaj, {imie} {nazwisko}!");
 
-            Console.Write("Podaj swój wiek: ");
+            //Console.Write("Podaj swój wiek: ");
             int wiek;
-            string s = Console.ReadLine();
+            string s = Interaction.InputBox("Podaj wiek ");  // Console.ReadLine();
             wiek = int.Parse(s);
 
             Console.WriteLine($"Masz {wiek} lat.");
 
             //jezeli wiek > 67 to jestes emerytem
             //a w przecinym przypadku do emerytury zostało Ci 67-wiek lat
-            if( wiek >= 67 )
+            if (wiek >= 67)
             {
                 Console.WriteLine(" jesteś emerytem");
             }
             else
             {
-                Console.WriteLine($" do emerytury zostało ci {67-wiek} lat");
+                Console.WriteLine($" do emerytury zostało ci {67 - wiek} lat");
             }
 
+            Interaction.MsgBox($"Witaj, {imie} {nazwisko} \n do emerytury zostało Ci {67-wiek} lat!");
         }
     }
 }
